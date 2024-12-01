795bc5583eca2a5f7e86fb62e503e2169dcfb8fc
EmmaH07 <harelemma07@gmail.com>
2024-12-01 10:30:39 +0200

    changed call to file

--- test_multiprocess.py
+++ test_multiprocess.py
@@ -2,29 +2,18 @@ from adv_db import AdvDB
 import multiprocessing
 
 
-def handle_single_process(dict_file_obj):
-    """
-
-    :param dict_file_obj: an object from the class AdvDb
-    :type dict_file_obj: AdvDB
-    :return:
-    """
-    key = input('Enter key: ')
-    val = input('Enter value: ')
-    print(dict_file_obj.set_val(key, val))
-    print(dict_file_obj.get_val(key))
-    print(dict_file_obj.delete_data(key))
-
-
 def handle_writing_process(dict_file_obj, key):
     """
 
     :param dict_file_obj: an object from the class AdvDb
-    :type dict_file_obj: AdvDB
-    :param key: the wanted key
+    :type dict_file_obj: AdvD
+    :param key: a fixed key to set
+    :type key: str
     :return:
     """
-    print(dict_file_obj.set_val(str(key), str(key)))
+    for i in range(100):
+        print(dict_file_obj.set_val(str(key), str(i)))
+        print(dict_file_obj.set_val(str(i), str(key)))
 
 
 def handle_reading_process(dict_file_obj, key):
@@ -33,9 +22,12 @@ def handle_reading_process(dict_file_obj, key):
     :param dict_file_obj: an object from the class AdvDb
     :type dict_file_obj: AdvDB
     :param key: the wanted key
+    :type key: str
     :return:
     """
-    print(dict_file_obj.get_val(str(key)))
+    for i in range(100):
+        print(dict_file_obj.get_val(key))
+        print(dict_file_obj.get_val(str(i)))
 
 
 def handle_deleting_process(dict_file_obj, key):
@@ -44,20 +36,21 @@ def handle_deleting_process(dict_file_obj, key):
     :param dict_file_obj: an object from the class AdvDb
     :type dict_file_obj: AdvDB
     :param key: the wanted key
+    :type key: str
     :return:
     """
-    print(dict_file_obj.delete_data(str(key)))
+    for i in range(100):
+        print(dict_file_obj.delete_data(str(key)))
+        print(dict_file_obj.delete_data(str(i)))
 
 
 def main():
     dict_file_obj = AdvDB(False)
-    proc = multiprocessing.Process(target=handle_single_process(dict_file_obj))
-    proc.start()
-    proc.join()
+    key = input('enter key: ')
     for i in range(40):
-        p1 = multiprocessing.Process(target=handle_writing_process(dict_file_obj, i))
-        p2 = multiprocessing.Process(target=handle_reading_process(dict_file_obj, i))
-        p3 = multiprocessing.Process(target=handle_deleting_process(dict_file_obj, i))
+        p1 = multiprocessing.Process(target=handle_writing_process(dict_file_obj, key))
+        p2 = multiprocessing.Process(target=handle_reading_process(dict_file_obj, key))
+        p3 = multiprocessing.Process(target=handle_deleting_process(dict_file_obj, key))
         p1.start()
         p2.start()
         p3.start()
